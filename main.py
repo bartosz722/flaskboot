44fe86d48a7879f7a4af777a01a619dcac4127f3
Bartosz <Bartosz@DESKTOP-6QG8RBQ>
2023-05-21 13:21:19 +0200

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,  render_template
+from flask import Flask, redirect,  render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,25 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/buy")
+def buy():
+    return render_template('buy.html')
+
+@app.route("/sell")
+def sell():
+    return render_template('sell.html')
+
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for('main'))
 
 @app.route("/some")
 def some():
